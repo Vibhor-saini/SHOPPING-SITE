c1cc7a57a71f51f4599b2b735a0dc640655bc02c
Vibhor saini <98160883+Vibhor-saini@users.noreply.github.com>
2022-07-03 15:36:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var $grid = $('.collection-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  var filterBtns = $('.filter-button-group').find('button');
+  function resetFilterBtns(){
+    filterBtns.each(function(){
+      $(this).removeClass('active-filter-btn');
+    });
+  }
+    
+    
